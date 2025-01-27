5da9ca5c2762d107192befbcb482f5fd4e840d90
camilahsp <116742754+camilahsp@users.noreply.github.com>
2025-01-27 10:20:49 -0400

    Arrumando erros.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+
+/*
 import { useEffect, useState } from 'react';
 import { Link, Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
+import AlterarProduto from './componentes/alterarproduto/AlterarProduto';
+
 
 type ProdutoType = {
   id: number;
@@ -37,7 +41,7 @@ function App() {
     fetch(`http://localhost:8000/produtos/${id}`, {
       method: 'DELETE',
     }).then((resposta) => {
-      if (resposta.status === 200) {
+      if (resposta.ok) {
         alert('Produto excluído com sucesso');
         window.location.reload();
       } else {
@@ -63,6 +67,7 @@ function App() {
           <Route path="/" element={<HomePage produtos={produtos} handleExcluir={handleExcluir} />} />
           <Route path="/usuarios" element={<UsuariosPage usuarios={usuarios} />} />
           <Route path="/cadastro-produto" element={<CadastroProdutoPage />} />
+          <Route path="/alterar-produto/:id" element={<AlterarProduto />} />
         </Routes>
       </div>
     </div>
@@ -143,43 +148,19 @@ function CadastroProdutoPage() {
       <form onSubmit={handleCadastroProduto}>
         <div>
           <label htmlFor="nome">Nome:</label>
-          <input
-            type="text"
-            id="nome"
-            value={nome}
-            onChange={(e) => setNome(e.target.value)}
-            required
-          />
+          <input type="text" id="nome" value={nome} onChange={(e) => setNome(e.target.value)} required />
         </div>
         <div>
           <label htmlFor="preco">Preço:</label>
-          <input
-            type="text"
-            id="preco"
-            value={preco}
-            onChange={(e) => setPreco(e.target.value)}
-            required
-          />
+          <input type="text" id="preco" value={preco} onChange={(e) => setPreco(e.target.value)} required />
         </div>
         <div>
           <label htmlFor="descricao">Descrição:</label>
-          <input
-            type="text"
-            id="descricao"
-            value={descricao}
-            onChange={(e) => setDescricao(e.target.value)}
-            required
-          />
+          <input type="text" id="descricao" value={descricao} onChange={(e) => setDescricao(e.target.value)} required />
         </div>
         <div>
           <label htmlFor="imagem">Imagem:</label>
-          <input
-            type="text"
-            id="imagem"
-            value={imagem}
-            onChange={(e) => setImagem(e.target.value)}
-            required
-          />
+          <input type="text" id="imagem" value={imagem} onChange={(e) => setImagem(e.target.value)} required />
         </div>
         <button type="submit">Cadastrar Produto</button>
       </form>
@@ -188,3 +169,403 @@ function CadastroProdutoPage() {
 }
 
 export default App;
+*/
+
+/*
+import { useEffect, useState } from 'react';
+import { Link, Route, Routes, useNavigate } from 'react-router-dom';
+import './App.css';
+import AlterarProduto from './componentes/alterarproduto/AlterarProduto';
+
+type ProdutoType = {
+  id: number;
+  nome: string;
+  preco: string;
+  descricao: string;
+  imagem: string;
+};
+
+type UsuarioType = {
+  id: number;
+  nome: string;
+  email: string;
+  created_at: string;
+  updated_at: string;
+};
+
+function App() {
+  const [produtos, setProdutos] = useState<ProdutoType[]>([]);
+  const [usuarios, setUsuarios] = useState<UsuarioType[]>([]);
+
+  useEffect(() => {
+    const fetchProdutos = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/produtos');
+        if (!response.ok) {
+          throw new Error('Erro ao buscar produtos');
+        }
+        const dados = await response.json();
+        setProdutos(dados);
+      } catch (error) {
+        console.error('Erro ao buscar produtos:', error);
+      }
+    };
+
+    const fetchUsuarios = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/usuarios');
+        if (!response.ok) {
+          throw new Error('Erro ao buscar usuários');
+        }
+        const dados = await response.json();
+        setUsuarios(dados);
+      } catch (error) {
+        console.error('Erro ao buscar usuários:', error);
+      }
+    };
+
+    fetchProdutos();
+    fetchUsuarios();
+  }, []);
+
+  const handleExcluir = async (id: number) => {
+    alert(`Excluir o produto com id ${id}`);
+    try {
+      const response = await fetch(`http://localhost:8000/produtos/${id}`, {
+        method: 'DELETE',
+      });
+      if (!response.ok) {
+        throw new Error('Erro ao excluir o produto');
+      }
+      alert('Produto excluído com sucesso');
+      window.location.reload();
+    } catch (error) {
+      console.error('Erro ao excluir o produto:', error);
+      alert('Erro ao excluir o produto: Confira o terminal do backend');
+    }
+  };
+
+  return (
+    <div className="app-container">
+      <header className="site-header">
+        <nav className="navigation">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/cadastro-produto">Cadastro de Produto</Link></li>
+            <li><Link to="/usuarios">Usuários</Link></li>
+          </ul>
+        </nav>
+      </header>
+
+      <div className="content-container">
+        <Routes>
+          <Route path="/" element={<HomePage produtos={produtos} handleExcluir={handleExcluir} />} />
+          <Route path="/usuarios" element={<UsuariosPage usuarios={usuarios} />} />
+          <Route path="/cadastro-produto" element={<CadastroProdutoPage />} />
+          <Route path="/alterar-produto/:id" element={<AlterarProduto />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+function HomePage({ produtos, handleExcluir }: { produtos: ProdutoType[], handleExcluir: (id: number) => void }) {
+  return (
+    <div className="produtos-container">
+      <h1 className="titulo-produto">Produtos</h1>
+      <div className="produtos-list">
+        {produtos.map((produto) => (
+          <div key={produto.id} className="produto-item">
+            <h3 className="produto-nome">{produto.nome}</h3>
+            <div className="container-imagem">
+              <img src={produto.imagem} alt="Imagem do produto" />
+            </div>
+            <p className="produto-preco">{produto.preco}</p>
+            <p className="produto-descricao">{produto.descricao}</p>
+            <button className="botao-comprar">Comprar</button>
+            <button onClick={() => handleExcluir(produto.id)}>Excluir</button>
+            <Link to={`/alterar-produto/${produto.id}`}>Alterar</Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function UsuariosPage({ usuarios }: { usuarios: UsuarioType[] }) {
+  return (
+    <div className="usuarios-container">
+      <h1 className="titulo-usuario">Usuários</h1>
+      <div className="usuarios-list">
+        {usuarios.map((usuario) => (
+          <div key={usuario.id} className="usuario-item">
+            <h2 className="usuario-nome">{usuario.nome}</h2>
+            <p>Email: {usuario.email}</p>
+            <p>Criado em: {new Date(usuario.created_at).toLocaleDateString()}</p>
+            <p>Atualizado em: {new Date(usuario.updated_at).toLocaleDateString()}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function CadastroProdutoPage() {
+  const [nome, setNome] = useState('');
+  const [preco, setPreco] = useState('');
+  const [descricao, setDescricao] = useState('');
+  const [imagem, setImagem] = useState('');
+  const navigate = useNavigate();
+
+  const handleCadastroProduto = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const produto = { nome, preco, descricao, imagem };
+
+    try {
+      const response = await fetch('http://localhost:8000/produtos', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(produto),
+      });
+
+      if (!response.ok) {
+        throw new Error('Erro ao cadastrar produto');
+      }
+
+      alert('Produto cadastrado com sucesso!');
+      navigate('/');
+    } catch (error) {
+      console.error('Erro ao cadastrar produto:', error);
+      alert('Erro ao cadastrar produto');
+    }
+  };
+
+  return (
+    <div className="cadastro-produto-container">
+      <h1>Cadastro de Produto</h1>
+      <form onSubmit={handleCadastroProduto}>
+        <div>
+          <label htmlFor="nome">Nome:</label>
+          <input type="text" id="nome" value={nome} onChange={(e) => setNome(e.target.value)} required />
+        </div>
+        <div>
+          <label htmlFor="preco">Preço:</label>
+          <input type="text" id="preco" value={preco} onChange={(e) => setPreco(e.target.value)} required />
+        </div>
+        <div>
+          <label htmlFor="descricao">Descrição:</label>
+          <input type="text" id="descricao" value={descricao} onChange={(e) => setDescricao(e.target.value)} required />
+        </div>
+        <div>
+          <label htmlFor="imagem">Imagem:</label>
+          <input type="text" id="imagem" value={imagem} onChange={(e) => setImagem(e.target.value)} required />
+        </div>
+        <button type="submit">Cadastrar Produto</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
+*/
+
+import { useEffect, useState } from 'react';
+import { Link, Route, Routes, useNavigate } from 'react-router-dom';
+import './App.css';
+import AlterarProduto from './componentes/alterarproduto/AlterarProduto';
+
+type ProdutoType = {
+  id: number;
+  nome: string;
+  preco: string;
+  descricao: string;
+  imagem: string;
+};
+
+type UsuarioType = {
+  id: number;
+  nome: string;
+  email: string;
+  created_at: string;
+  updated_at: string;
+};
+
+function App() {
+  const [produtos, setProdutos] = useState<ProdutoType[]>([]);
+  const [usuarios, setUsuarios] = useState<UsuarioType[]>([]);
+
+  useEffect(() => {
+    const fetchProdutos = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/produtos');
+        if (!response.ok) {
+          throw new Error('Erro ao buscar produtos');
+        }
+        const dados = await response.json();
+        setProdutos(dados);
+      } catch (error) {
+        console.error('Erro ao buscar produtos:', error);
+      }
+    };
+
+    const fetchUsuarios = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/usuarios');
+        if (!response.ok) {
+          throw new Error('Erro ao buscar usuários');
+        }
+        const dados = await response.json();
+        setUsuarios(dados);
+      } catch (error) {
+        console.error('Erro ao buscar usuários:', error);
+      }
+    };
+
+    fetchProdutos();
+    fetchUsuarios();
+  }, []);
+
+  const handleExcluir = async (id: number) => {
+    alert(`Excluir o produto com id ${id}`);
+    try {
+      const response = await fetch(`http://localhost:8000/produtos/${id}`, {
+        method: 'DELETE',
+      });
+      if (!response.ok) {
+        throw new Error('Erro ao excluir o produto');
+      }
+      alert('Produto excluído com sucesso');
+      window.location.reload();
+    } catch (error) {
+      console.error('Erro ao excluir o produto:', error);
+      alert('Erro ao excluir o produto: Confira o terminal do backend');
+    }
+  };
+
+  return (
+    <div className="app-container">
+      <header className="site-header">
+        <nav className="navigation">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/cadastro-produto">Cadastro de Produto</Link></li>
+            <li><Link to="/usuarios">Usuários</Link></li>
+          </ul>
+        </nav>
+      </header>
+
+      <div className="content-container">
+        <Routes>
+          <Route path="/" element={<HomePage produtos={produtos} handleExcluir={handleExcluir} />} />
+          <Route path="/usuarios" element={<UsuariosPage usuarios={usuarios} />} />
+          <Route path="/cadastro-produto" element={<CadastroProdutoPage />} />
+          <Route path="/alterar-produto/:id" element={<AlterarProduto />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+function HomePage({ produtos, handleExcluir }: { produtos: ProdutoType[], handleExcluir: (id: number) => void }) {
+  return (
+    <div className="produtos-container">
+      <h1 className="titulo-produto">Produtos</h1>
+      <div className="produtos-list">
+        {produtos.map((produto) => (
+          <div key={produto.id} className="produto-item">
+            <h3 className="produto-nome">{produto.nome}</h3>
+            <div className="container-imagem">
+              <img src={produto.imagem} alt="Imagem do produto" />
+            </div>
+            <p className="produto-preco">{produto.preco}</p>
+            <p className="produto-descricao">{produto.descricao}</p>
+            <button className="botao-comprar">Comprar</button>
+            <button onClick={() => handleExcluir(produto.id)}>Excluir</button>
+            <Link to={`/alterar-produto/${produto.id}`}>Alterar</Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function UsuariosPage({ usuarios }: { usuarios: UsuarioType[] }) {
+  return (
+    <div className="usuarios-container">
+      <h1 className="titulo-usuario">Usuários</h1>
+      <div className="usuarios-list">
+        {usuarios.map((usuario) => (
+          <div key={usuario.id} className="usuario-item">
+            <h2 className="usuario-nome">{usuario.nome}</h2>
+            <p>Email: {usuario.email}</p>
+            <p>Criado em: {new Date(usuario.created_at).toLocaleDateString()}</p>
+            <p>Atualizado em: {new Date(usuario.updated_at).toLocaleDateString()}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function CadastroProdutoPage() {
+  const [nome, setNome] = useState('');
+  const [preco, setPreco] = useState('');
+  const [descricao, setDescricao] = useState('');
+  const [imagem, setImagem] = useState('');
+  const navigate = useNavigate();
+
+  const handleCadastroProduto = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const produto = { nome, preco, descricao, imagem };
+
+    try {
+      const response = await fetch('http://localhost:8000/produtos', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(produto),
+      });
+
+      if (!response.ok) {
+        throw new Error('Erro ao cadastrar produto');
+      }
+
+      alert('Produto cadastrado com sucesso!');
+      navigate('/');
+    } catch (error) {
+      console.error('Erro ao cadastrar produto:', error);
+      alert('Erro ao cadastrar produto');
+    }
+  };
+
+  return (
+    <div className="cadastro-produto-container">
+      <h1>Cadastro de Produto</h1>
+      <form onSubmit={handleCadastroProduto}>
+        <div>
+          <label htmlFor="nome">Nome:</label>
+          <input type="text" id="nome" value={nome} onChange={(e) => setNome(e.target.value)} required />
+        </div>
+        <div>
+          <label htmlFor="preco">Preço:</label>
+          <input type="text" id="preco" value={preco} onChange={(e) => setPreco(e.target.value)} required />
+        </div>
+        <div>
+          <label htmlFor="descricao">Descrição:</label>
+          <input type="text" id="descricao" value={descricao} onChange={(e) => setDescricao(e.target.value)} required />
+        </div>
+        <div>
+          <label htmlFor="imagem">Imagem:</label>
+          <input type="text" id="imagem" value={imagem} onChange={(e) => setImagem(e.target.value)} required />
+        </div>
+        <button type="submit">Cadastrar Produto</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
+
--- src/componentes/alterarproduto/AlterarProduto.tsx
+++ src/componentes/alterarproduto/AlterarProduto.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+/* import { useParams } from "react-router-dom";
 import { ChangeEvent, FormEvent, useState , useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 function AlterarProduto(){
@@ -90,6 +90,96 @@ function AlterarProduto(){
             </form>
         </>
     )
+}
+
+export default AlterarProduto;
+
+*/ 
+
+import { useParams } from "react-router-dom";
+import { ChangeEvent, FormEvent, useState , useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+
+function AlterarProduto() {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [nome, setNome] = useState("");
+    const [descricao, setDescricao] = useState("");
+    const [preco, setPreco] = useState("");
+    const [imagem, setImagem] = useState("");
+
+    useEffect(() => {
+        fetch(`http://localhost:8000/produtos/${id}`)
+            .then(resposta => resposta.json())
+            .then(dados => {
+                setNome(dados.nome);
+                setDescricao(dados.descricao);
+                setPreco(dados.preco);
+                setImagem(dados.imagem);
+            })
+            .catch(() => {
+                alert("Erro ao buscar dados do produto.");
+            });
+    }, [id]);
+
+    async function handleForm(event: FormEvent) {
+        event.preventDefault();
+        try {
+            const resposta = await fetch(`http://localhost:8000/produtos/${id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    nome: nome,
+                    descricao: descricao,
+                    preco: preco,
+                    imagem: imagem
+                })
+            });
+            if (resposta.ok) {
+                alert("Produto alterado com sucesso");
+                navigate("/");
+            } else {
+                const mensagem = await resposta.text();
+                alert("Erro ao alterar produto: " + mensagem);
+            }
+        } catch (e) {
+            alert("Servidor não está respondendo.");
+        }
+    }
+
+    return (
+        <>
+            <h1>Alterar Produto</h1>
+            <form onSubmit={handleForm}>
+                <div>
+                    <label htmlFor="id">Id</label>
+                    <input placeholder="Id" type="text" name="id" id="id" value={id} readOnly />
+                </div>
+                <div>
+                    <label htmlFor="nome">Nome</label>
+                    <input placeholder="Nome" type="text" name="nome" id="nome" value={nome} onChange={(e) => setNome(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="descricao">Descrição</label>
+                    <input placeholder="Descrição" type="text" name="descricao" id="descricao" value={descricao} onChange={(e) => setDescricao(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="preco">Preço</label>
+                    <input placeholder="Preço" type="text" name="preco" id="preco" value={preco} onChange={(e) => setPreco(e.target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="imagem">URL Imagem</label>
+                    <input placeholder="URL Imagem" type="text" name="imagem" id="imagem" value={imagem} onChange={(e) => setImagem(e.target.value)} />
+                    {imagem && <img className="imagem-produto-reduzida" src={imagem} alt="Imagem do Produto" />}
+                </div>
+                <div>
+                    <input type="submit" value="Alterar" />
+                </div>
+            </form>
+        </>
+    );
 }
 
 export default AlterarProduto;
